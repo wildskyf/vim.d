3ce7ededf37fe20807c074110d88c11deef07efd
wildskyf <poppin.wildsky+gh@gmail.com>
2024-12-24 01:18:23 +0800

    add codecompanion

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
-  --disable mini.pairs
   { "echasnovski/mini.pairs", enabled = false },
+  { "neovim/nvim-lspconfig", enabled = false },
+  { "mason.nvim", enabled = false },
+  -- { "saghen/blink.cmp", enabled = false },
 }
