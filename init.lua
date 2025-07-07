4c2e39ddbf459f617251279ef122b0eed7eb044a
wildskyf <poppin.wildsky+gh@gmail.com>
2025-07-07 11:00:59 +0800

    remove useless things

--- init.lua
+++ init.lua
@@ -4,23 +4,4 @@ if vim.g.vscode then
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
-
-  require("codecompanion").setup({
-    adapters = {
-      ollama = function()
-        return require("codecompanion.adapters").extend("ollama", {
-          env = {
-            url = "http://127.0.0.1:11434", -- optional: default value is ollama url http://127.0.0.1:11434
-            api_key = "",
-          },
-          headers = {
-            ["Content-Type"] = "application/json",
-          },
-          parameters = {
-            sync = true,
-          },
-        })
-      end,
-    },
-  })
 end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,4 @@
 
 vim.opt.relativenumber = false
 vim.g.lazyvim_python_ruff = "ruff"
+vim.g.snacks_animate = false
