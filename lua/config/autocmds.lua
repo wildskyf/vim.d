760d5ce23a83566f44d38eece44eedad8d94f716
wildskyf <poppin.wildsky+gh@gmail.com>
2025-07-18 13:21:56 +0800

    disabled autoformat

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,6 +6,6 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "*" },
   callback = function()
-    -- vim.b.autoformat = false
+    vim.b.autoformat = false
   end,
 })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@
 vim.opt.relativenumber = false
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.snacks_animate = false
+
+-- Auto-detect file indentation settings
+vim.opt.modeline = true -- Enable modeline detection
+vim.opt.modelines = 5 -- Check first/last 5 lines for modelines
+vim.opt.autoindent = true -- Copy indent from current line when starting new line
+vim.opt.smartindent = true -- Smart autoindenting for C-like programs
