fa9f35accd00beb09a2edeeeba2fcbf89eaa4650
wildskyf <poppin.wildsky+gh@gmail.com>
2025-09-10 14:50:16 +0800

    adjust keympa

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,39 +4,41 @@
 
 local map = vim.keymap.set
 
-map("n", "<HOME>", "^", { desc = "Move Cursor To line head"    })
+map("n", "<HOME>", "^", { desc = "Move Cursor To line head" })
 
 -- move cursor faster
-map("n", "<C-k>",     "5gk", { desc = "Move Up Cursor 5 Lines"    })
-map("n", "<C-UP>",    "5gk", { desc = "Move Up Cursor 5 Lines"    })
-map("n", "<C-j>",     "5gj", { desc = "Move Down Cursor 5 Lines"  })
-map("n", "<C-DOWN>",  "5gj", { desc = "Move Down Cursor 5 Lines"  })
-map("n", "<C-h>",     "b",   { desc = "Move Cursor To Word Begin" })
-map("n", "<C-LEFT>",  "b",   { desc = "Move Cursor To Word Begin" })
-map("n", "<C-l>",     "e",   { desc = "Move Cursor To Word End"   })
-map("n", "<C-RIGHT>", "e",   { desc = "Move Cursor To Word End"   })
+map("n", "<C-k>", "5gk", { desc = "Move Up Cursor 5 Lines" })
+map("n", "<C-UP>", "5gk", { desc = "Move Up Cursor 5 Lines" })
+map("n", "<C-j>", "5gj", { desc = "Move Down Cursor 5 Lines" })
+map("n", "<C-DOWN>", "5gj", { desc = "Move Down Cursor 5 Lines" })
+map("n", "<C-h>", "b", { desc = "Move Cursor To Word Begin" })
+map("n", "<C-LEFT>", "b", { desc = "Move Cursor To Word Begin" })
+map("n", "<C-l>", "e", { desc = "Move Cursor To Word End" })
+map("n", "<C-RIGHT>", "e", { desc = "Move Cursor To Word End" })
 
 -- adjust window size
-map("n", "<leader><up>",    ":5winc +<CR>",   { desc = "" })
-map("n", "<leader><down>",  ":5winc -<CR>",   { desc = "" })
-map("n", "<leader><left>",  ":5winc <<CR>",   { desc = "" })
-map("n", "<leader><right>", ":5winc ><CR>",   { desc = "" })
+map("n", "<leader><up>", ":5winc +<CR>", { desc = "Adjust Window Size (up +5)" })
+map("n", "<leader><down>", ":5winc -<CR>", { desc = "Adjust Window Size (down +5)" })
+map("n", "<leader><left>", ":5winc <<CR>", { desc = "Adjust Window Size (left +5)" })
+map("n", "<leader><right>", ":5winc ><CR>", { desc = "Adjust Window Size (right +5)" })
 
 -- ripgrep integrated
-map("n", "<leader>r",       ":Rg<space>",     { desc = "" })
+map("n", "<leader>r", ":Rg<space>", { desc = "" })
 
 -- move lines
-map("n", "<S-DOWN>", "<cmd>m .+1<cr>==",        { desc = "Move Down" })
+map("n", "<S-DOWN>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
 map("i", "<S-DOWN>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("v", "<S-DOWN>", ":m '>+1<cr>gv=gv",        { desc = "Move Down" })
-map("n", "<S-UP>", "<cmd>m .-2<cr>==",          { desc = "Move Up" })
-map("i", "<S-UP>", "<esc><cmd>m .-2<cr>==gi",   { desc = "Move Up" })
-map("v", "<S-UP>", ":m '<-2<cr>gv=gv",          { desc = "Move Up" })
+map("v", "<S-DOWN>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("n", "<S-UP>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<S-UP>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<S-UP>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
+-- buffer navigation
+map("n", "<S-LEFT>", "<cmd>bprevious<cr>", { desc = "Privious Buffer" })
+map("n", "<S-RIGHT>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 
 -- Archives
 
 -- buffer is now bind with tabs
--- map("n", "<leader><Tab>",   ":bnext<CR>",     { desc = "" })
+-- map("n", "<leader><Tab>", ":bnext<CR>", { desc = "" })
 -- map("n", "<leader><S-Tab>", ":bprevious<CR>", { desc = "" })
-
